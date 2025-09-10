9cc6102c144b5033f35e5b3b45982845a6ffbf8d
SAMIR-U <samir.borda@uptc.edu.co>
2025-09-09 21:28:50 -0500

    attribute tail, methods add, get, size, show, __str__ implemented

--- src/DoubleList.py
+++ src/DoubleList.py
@@ -3,15 +3,36 @@ from Node import DoubleNode
 class DoubleList:
     def __init__(self):
         self.head:DoubleNode = None
+        self.tail:DoubleNode = None
     
     def add(self,data):
-        pass
+        if self.head is None: # esto se cambia por self.is_empty()
+            self.head = DoubleNode(data)
+            self.tail = self.head
+            return
+        self.tail.next = DoubleNode(data)
+        self.tail = self.tail.next
+        
 
-    def get(self,index):
-        pass
+    def get(self,index): 
+        if self.head is None:# esto se cambia por self.is_empty()
+            return None
+        if index<0 or index>=self.size():
+            raise IndexError("Index out of range (index<0 or index>=self.size())")
+        current = self.head
+        for i in range(index):
+            current = current.next
+        return current.value
 
-    def size(self):
-        pass
+    def size(self): 
+        if self.head is None: # esto se cambia por self.is_empty()
+            return 0
+        count = 0
+        current = self.head
+        while current is not None:
+            current = current.next
+            count+=1
+        return count
     
     def exist(self,data):
         pass
@@ -23,9 +44,7 @@ class DoubleList:
         pass
 
     def show(self):
-        pass
+        return f"[{self.head}]"
 
     def __str__(self):
-        pass
-
-
+        return self.show()
--- src/Test.py
+++ src/Test.py
@@ -1,3 +1,14 @@
 from DoubleList import DoubleList
 
 my_list = DoubleList() 
+my_list.add("first")
+my_list.add("test")
+my_list.add("test")
+my_list.add("test")
+my_list.add("test")
+my_list.add("last")
+
+print(my_list)
+print(my_list.size())
+print(my_list.get(0))
+print(my_list.get(my_list.size()-1))
